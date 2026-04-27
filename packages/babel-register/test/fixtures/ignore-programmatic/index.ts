ed587c3bfc8a93413fce01288f194d24b957e4a6
Huáng Jùnliàng <jlhwung@gmail.com>
2026-04-27 10:30:40 -0400

    fix(register): skip hook for ignored files (#17928)

---
+++ packages/babel-register/test/fixtures/ignore-programmatic/index.ts
@@ -0,0 +1,26 @@
+const register = require('../../..').default;
+
+register( {
+  ignore: ["./ignored/"],
+  babelrc: false,
+  configFile: false,
+  extensions: [".ts"],
+  presets: ["@babel/preset-typescript"],
+} );
+
+// handled by Babel
+require("./loaded.ts");
+
+// handled by node
+require("./ignored/ignored-pass.ts");
+
+// handled by node and should throw SyntaxError when loaded by node
+try {
+  require("./ignored/ignored-throw.ts");
+} catch (e) {
+  if (e instanceof SyntaxError && e.message.includes("TypeScript enum is not supported in strip-only mode")) {
+    process.stdout.write("SyntaxError is thrown as expected from ignored-throw.ts");
+  } else {
+    throw e;
+  }
+}
